849b5b55295064cef7e8049454d44961bb36015d
felipesinn <felipesinn01@gmail.com>
2023-08-15 23:41:03 -0300

    atividade revisadda em aula/ ref Lucas Santos.

---
+++ script.js
@@ -0,0 +1,170 @@
+const users = [
+    {
+      username: "Diener",
+      password: "passwd",
+      tasks: [],
+    },
+  ];
+  
+  function validateIndex(index, user) {
+    return !index || isNaN(index) || index < 0 || index >= user.tasks.length;
+  }
+  
+  function createTask(title, description, user) {
+    if (title === "" || title === undefined) {
+      console.log("O título é obrigatório");
+      return;
+    }
+    user.tasks.push({ title: title, description, completed: false });
+  }
+  
+  function listTasks(user) {
+    user.tasks.forEach((tarefa, index) => {
+      console.log(
+        `${index} - Título: ${tarefa.title} - Descrição: ${
+          tarefa.description
+        } - Tarefa completada: ${tarefa.completed ? "Sim" : "Não"}`
+      );
+    });
+  }
+  
+  function updateTaskStatus(index, completed, user) {
+    if (validateIndex(index, user)) {
+      console.log("Índice não encontrado!");
+      return;
+    }
+  
+    user.tasks[index].completed = completed;
+  }
+  
+  function updateTask(index, newTitle, newDescription, user) {
+    if (validateIndex(index, user)) {
+      console.log("Índice não encontrado!");
+      return;
+    }
+  
+    if (!newTitle) {
+      console.log("O título é obrigatório");
+      return;
+    }
+  
+    user.tasks[index].title = newTitle;
+    user.tasks[index].description = newDescription;
+  }
+  
+  function deleteTask(index, user) {
+    if (validateIndex(index, user)) {
+      console.log("Índice não encontrado!");
+      return;
+    }
+  
+    user.tasks.splice(index, 1);
+  }
+  
+  // ------------------ User e Login ---------------
+  
+  function createUser(username, password) {
+    if (!username || !password) {
+      console.log("Gentileza informar o nome do usuário e/ou a Senha");
+      return;
+    }
+    users.push({ username, password, tasks: [] });
+  }
+  
+  function login(username, password) {
+    const usuario = users.find((user) => user.username === username);
+    if (!usuario) {
+      console.log("Usuário não encontrado!");
+      return;
+    }
+    if (usuario.password !== password) {
+      console.log("Senha errada.");
+      return;
+    }
+    console.log(usuario);
+    return usuario;
+  }
+  
+  function managerTasks(user) {
+    do {
+      switch (
+        prompt(`Digite uma opção:
+    1 - Cadastrar tarefa
+    2 - Atualizar status tarefa
+    3 - Atualizar tarefa
+    4 - Listar tarefas
+    5 - Excluir tarefa
+    `)
+      ) {
+        case "1":
+          createTask(
+            prompt("Digite o título"),
+            prompt("Escreva a descrição"),
+            user
+          );
+          break;
+  
+        case "2":
+          updateTaskStatus(
+            parseInt(prompt("Digite o índice da tarefa:")),
+            prompt(
+              "Se a tarefa foi concluída, digite sim, se não digite não."
+            ) === "sim"
+              ? true
+              : false,
+            user
+          );
+          break;
+  
+        case "3":
+          updateTask(
+            parseInt(prompt("Digite o índice da tarefa:")),
+            prompt("Digite o título"),
+            prompt("Escreva a descrição"),
+            user
+          );
+          break;
+  
+        case "4":
+          listTasks(user);
+          break;
+  
+        case "5":
+          let tarefaDeletada = prompt(
+            "Qual o index da tarefa que deseja deletar?"
+          );
+          deleteTask(tarefaDeletada, user);
+        default:
+          break;
+      }
+    } while (confirm("Deseja continuar neste usuario?"));
+  }
+  
+  function inicio() {
+    do {
+      switch (prompt("O que deseja fazer: 1 - Cadastro, 2 - Login")) {
+        case "1":
+          createUser(
+            prompt("Digite o nome do usuário"),
+            prompt("Digite a senha")
+          );
+          break;
+  
+        case "2":
+          const user = login(
+            prompt("Digite o nome do usuário"),
+            prompt("Digite a senha")
+          );
+          if (user) {
+            managerTasks(user);
+          }
+          break;
+  
+        default:
+          break;
+      }
+    } while (confirm("Deseja continuar"));
+  }
+  
+  // inicio do programa
+  inicio();
